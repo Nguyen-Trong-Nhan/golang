631a06f27ee6cb620f15e84d456fedc1285eafaf
nguyentrongnhan2605 <50715022+Nguyen-Trong-Nhan@users.noreply.github.com>
2019-11-11 02:44:55 +0700

    new practices

--- hello/hello.go
+++ hello/hello.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Le Thi Ha Lan 76\n")
-	fmt.Printf("Nguyen Trong Nhan 67\n")
+	fmt.Println("Le Thi Ha Lan 76\n")
+	fmt.Println("Nguyen Trong Nhan 67\n")
 }
+
+//Printf (symbols, argument) ex: Printf("%s", "Hello")
+//Println (raw string) ex: Println("Hello")
